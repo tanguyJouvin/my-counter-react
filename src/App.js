0232e4c0772de8a30b7d25548dd5f1dc3a59907d
Tanguy Jouvin <tanguyj28@yahoo.fr>
2019-05-08 16:56:23 +0200

    some adds and fixed style bugs

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Container, Row, Col } from 'reactstrap';
 import './App.css';
 import Counter from './Components/Counter/Counter';
 
@@ -6,10 +7,15 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          <Counter />
-          
-        </p>
+        <Container>
+          <Row>
+            <Col xs="auto" md='6' xl='4'>
+              <p>
+                <Counter data="reset" name="visitor"/>
+              </p>
+            </Col>
+          </Row>
+        </Container>
       </header>
     </div>
   );
--- src/Components/Counter/Counter.js
+++ src/Components/Counter/Counter.js
@@ -16,19 +16,20 @@ class Counter extends Component {
       this.setState({counter: this.state.counter - 1});
     }
   }
-  reset = () => {
+  resetCounter = () => {
     this.setState({counter: 0});
   }
   render() {
     return (
       <div>
         <Card body inverse w-100 style={{ backgroundColor: '#222', borderColor: '#333',borderStyle: 'inset', borderRadius: '5px 10px / 2em 3em', borderWidth: '0.5em 0.2em 0.5em 0.2em'}}>
+          <p style={{fontFamily: 'Roboto',}}><b  className="text-info">Welcome</b> {this.props.name}</p>
           <p style={{fontFamily: 'Roboto',}}><b  className="text-info">counter :</b> [{this.state.counter}]</p>
           <Row noGutters>
-            <Col><Button style={{backgRoundColor: '#004D40', width: '5em', borderColor: '#0097A7', borderWidth: '0.1em 0.2em 0.1em 0.2em'}} active onClick={this.add}>+</Button></Col>
-            <Col><Button style={{backgroundColor: '#005D40', width: '5em', borderColor: '#0097A7', borderWidth: '0.1em 0.2em 0.1em 0.2em'}} active onClick={this.substract}>-</Button></Col>
+            <Col><Button style={{backgRoundColor: '#004D40', width: '100%', borderColor: '#0097A7', borderWidth: '0.1em 0.2em 0.1em 0.2em'}} active onClick={this.add}>+</Button></Col>
+            <Col><Button style={{backgroundColor: '#005D40', width: '100%', borderColor: '#0097A7', borderWidth: '0.1em 0.2em 0.1em 0.2em'}} active onClick={this.substract}>-</Button></Col>
           </Row>
-          <Button style={{color:'#009688'}} size="md" onClick={this.reset}>reset</Button>
+          <Button style={{color:'#009688'}} size="md" onClick={this.resetCounter}>{this.props.data}</Button>
         </Card>
       </div>
     )
