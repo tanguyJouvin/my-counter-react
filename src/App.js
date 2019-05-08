9b9b1846dc5799f5f5f6982f471e131c185f35c9
Tanguy Jouvin <tanguyj28@yahoo.fr>
2019-05-08 15:17:23 +0200

    realise a counter designed with reactstrap

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import Counter from './Components/Counter/Counter';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          <Counter />
+          
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Counter/Counter.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import { Card,Button, Row, Col } from 'reactstrap';
+
+class Counter extends Component {
+  constructor() {
+    super();
+    this.state = {
+     counter: 0
+    }
+  }
+  add = () => {
+    this.setState({counter: this.state.counter + 1}); 
+  }
+  substract = () => {
+    if(this.state.counter > 0) {
+      this.setState({counter: this.state.counter - 1});
+    }
+  }
+  reset = () => {
+    this.setState({counter: 0});
+  }
+  render() {
+    return (
+      <div>
+        <Card body inverse w-100 style={{ backgroundColor: '#222', borderColor: '#333',borderStyle: 'inset', borderRadius: '5px 10px / 2em 3em', borderWidth: '0.5em 0.2em 0.5em 0.2em'}}>
+          <p style={{fontFamily: 'Roboto',}}><b  className="text-info">counter :</b> [{this.state.counter}]</p>
+          <Row noGutters>
+            <Col><Button style={{backgRoundColor: '#004D40', width: '5em', borderColor: '#0097A7', borderWidth: '0.1em 0.2em 0.1em 0.2em'}} active onClick={this.add}>+</Button></Col>
+            <Col><Button style={{backgroundColor: '#005D40', width: '5em', borderColor: '#0097A7', borderWidth: '0.1em 0.2em 0.1em 0.2em'}} active onClick={this.substract}>-</Button></Col>
+          </Row>
+          <Button style={{color:'#009688'}} size="md" onClick={this.reset}>reset</Button>
+        </Card>
+      </div>
+    )
+  }
+}
+
+
+export default Counter;
